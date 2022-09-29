f51d74105af81e697039178862e91c67a34ec1eb
Julio Cesar Castillo <famcas48@gmail.com>
2022-09-29 14:17:25 -0400

    Set up webpack and wire things up for webpack to work

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,18 @@
 import express from "express";//var express = require('express');
 import path from "path";//var path = require('path');
 import open from "open"; //var open = require('open');
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+//Integrate webpack with express
+app.use(require('webpack-dev-middleware')(compiler, {
+   /*noInfo: true,*/
+   publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res){
    res.sendFile(path.join(__dirname, '../src/index.html'));
