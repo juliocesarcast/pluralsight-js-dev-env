d496c74d685753a97530104a64aa795f5f16c830
Julio Cesar Castillo <famcas48@gmail.com>
2022-09-29 15:54:31 -0400

    Add linting support on save files

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from "chalk"; //var chalk = require('chalk'); //Chalk is used to display colored log messages
 
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
