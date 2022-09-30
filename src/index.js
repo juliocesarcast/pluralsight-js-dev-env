0450d33efbfec3d339d1c4321ccf29b164277de3
Julio Cesar Castillo <famcas48@gmail.com>
2022-09-30 12:09:07 -0400

    Fix linter issues

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './index.css';
 
-//import numeral from 'numeral';
+import numeral from 'numeral';
 
-//const courseValue = numeral(1000).format('$0,0.00');
+const courseValue = numeral(1000).format('$0,0.00');
 //debugger;
 
-//console.log(`I would pay ${courseValue} for this awesome course!`);
+/* eslint-disable no-console */
+console.info(`I would pay ${courseValue} for this awesome course!`);
